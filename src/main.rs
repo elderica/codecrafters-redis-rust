5d25498c54c8d2028ce5c7b9d5f59a431bd8cf64
elderica <1130138+elderica@users.noreply.github.com>
2023-01-17 21:31:58 +0900

    pass: Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use std::net::TcpListener;
+use std::io::{self, Read, Write};
+use std::{net, result};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn split_resp(resp: &[u8]) -> Vec<String> {
+    resp.split(|b| *b == 0x0d || *b == 0x0a)
+        .map(|s| String::from_utf8(s.to_vec()).expect("should not be invalid bytes for utf8"))
+        .collect()
+}
+
+const PONG: &[u8] = "+PONG\r\n".as_bytes();
+
+fn main() -> result::Result<(), io::Error> {
+    let listener = net::TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                eprintln!("accepted new connection");
+
+                let mut buf = [0; 64];
+                _stream.read(&mut buf)?;
+
+                let parsed = split_resp(&buf);
+                eprintln!("{:?}", parsed);
+
+                _stream.write(PONG)?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
