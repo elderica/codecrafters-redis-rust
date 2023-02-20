3d466e7fc8a231ada09b7e485c5dc8dc4d57fd81
elderica <1130138+elderica@users.noreply.github.com>
2023-02-20 20:40:14 +0900

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,35 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
-
-// fn split_resp(resp: &[u8]) -> Vec<String> {
-//     resp.split(|b| *b == 0x0d || *b == 0x0a)
-//         .map(|s| String::from_utf8(s.to_vec()).expect("should not be invalid bytes for utf8"))
-//         .collect()
-// }
-
-const PONG: &[u8] = "+PONG\r\n".as_bytes();
-
-fn main() -> std::result::Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:6379")?;
-
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6379").await?;
     loop {
-        let (stream, addr) = listener.accept()?;
+        let (stream, addr) = listener.accept().await?;
         eprintln!("accepted: {addr:?}");
-        handle_one_stream(stream);
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                eprintln!("Err({e:?}");
+            }
+        });
     }
 }
 
-fn handle_one_stream(stream: std::net::TcpStream) {
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
+const PONG: &[u8] = "+PONG\r\n".as_bytes();
 
+async fn handle_connection(mut stream: tokio::net::TcpStream) -> tokio::io::Result<()> {
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+    let (mut rx, mut tx) = stream.split();
+    let mut buf = [0; 4096];
     loop {
-        match reader.fill_buf() {
-            Ok(buf) => {
-                if buf.is_empty() {
-                    return;
-                }
-                if let Err(e) = writer.write_all(PONG) {
-                    eprintln!("failed to write_all: {e:?}");
-                    return;
-                }
-                if let Err(e) = writer.flush() {
-                    eprint!("failed to flush: {e:?}");
-                    return;
-                }
-                let len = buf.len();
-                reader.consume(len);
+        match rx.read(&mut buf).await {
+            Ok(0) => {
+                return Ok(());
+            }
+            Ok(_) => {
+                tx.write_all(PONG).await?;
+                tx.flush().await?;
             }
             Err(e) => {
-                eprintln!("failed to fill_buf: {e:?}");
-                return;
+                return Err(e);
             }
         }
     }
