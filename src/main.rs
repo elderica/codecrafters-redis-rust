f965301f44fc6c8bb5a181ad5dadacaf8a822766
elderica <1130138+elderica@users.noreply.github.com>
2023-02-20 19:41:56 +0900

    respond to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
-use std::io::{self, Read, Write};
-use std::{net, result};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
-fn split_resp(resp: &[u8]) -> Vec<String> {
-    resp.split(|b| *b == 0x0d || *b == 0x0a)
-        .map(|s| String::from_utf8(s.to_vec()).expect("should not be invalid bytes for utf8"))
-        .collect()
-}
+// fn split_resp(resp: &[u8]) -> Vec<String> {
+//     resp.split(|b| *b == 0x0d || *b == 0x0a)
+//         .map(|s| String::from_utf8(s.to_vec()).expect("should not be invalid bytes for utf8"))
+//         .collect()
+// }
 
 const PONG: &[u8] = "+PONG\r\n".as_bytes();
 
-fn main() -> result::Result<(), io::Error> {
-    let listener = net::TcpListener::bind("127.0.0.1:6379")?;
+fn main() -> std::result::Result<(), std::io::Error> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:6379")?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                eprintln!("accepted new connection");
+    loop {
+        let (stream, addr) = listener.accept()?;
+        eprintln!("accepted: {addr:?}");
+        handle_one_stream(stream);
+    }
+}
 
-                let mut buf = [0; 64];
-                _stream.read_exact(&mut buf)?;
+fn handle_one_stream(stream: std::net::TcpStream) {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
 
-                let parsed = split_resp(&buf);
-                eprintln!("{:?}", parsed);
-
-                _stream.write_all(PONG)?;
+    loop {
+        match reader.fill_buf() {
+            Ok(buf) => {
+                if buf.is_empty() {
+                    return;
+                }
+                if let Err(e) = writer.write_all(PONG) {
+                    eprintln!("failed to write_all: {e:?}");
+                    return;
+                }
+                if let Err(e) = writer.flush() {
+                    eprint!("failed to flush: {e:?}");
+                    return;
+                }
+                let len = buf.len();
+                reader.consume(len);
             }
             Err(e) => {
-                eprintln!("error: {}", e);
+                eprintln!("failed to fill_buf: {e:?}");
+                return;
             }
         }
     }
-
-    Ok(())
 }
